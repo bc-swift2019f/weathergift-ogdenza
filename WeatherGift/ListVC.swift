71e7966275102197edfe86ab3b14af4c5ff564b7
Zach Ogden <ogdenza@bc.edu>
2019-09-24 23:12:35 -0400

    7.3 Completed

--- WeatherGift/ListVC.swift
+++ WeatherGift/ListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ListVC.swift
 //  WeatherGift
 //
 //  Created by Zach Ogden on 9/24/19.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
-
---
+++ WeatherGift/PageVC.swift
@@ -0,0 +1,61 @@
+//
+//  PageVC.swift
+//  WeatherGift
+//
+//  Created by Zach Ogden on 9/24/19.
+//  Copyright © 2019 Zach Ogden. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIPageViewController {
+    
+    var currentPage = 0
+    var locationsArray = ["Local City", "Sydney, Australia", "Accra Ghana", "Uglich Russia"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+        dataSource = self
+        
+        setViewControllers([createDetailVC(forPage: 0)], direction: .forward, animated: false, completion: nil )
+
+    }
+    
+    func createDetailVC(forPage page: Int) -> DetailVC {
+        currentPage = min(max(0, page), locationsArray.count - 1)
+        
+        let detailVC = storyboard!.instantiateViewController(withIdentifier: "DetailVC") as! DetailVC
+        
+        detailVC.locationsArray = locationsArray
+        detailVC.currentPage = currentPage 
+        
+        
+        return detailVC
+    }
+    
+    
+    
+}
+
+extension PageVC: UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        if let currentViewController = viewController as? DetailVC {
+            if currentViewController.currentPage < locationsArray.count - 1 {
+                return createDetailVC(forPage: currentViewController.currentPage + 1)
+            }
+        }
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let currentViewController = viewController as? DetailVC {
+            if currentViewController.currentPage > 0 {
+                return createDetailVC(forPage: currentViewController.currentPage - 1)
+            }
+        }
+        return nil
+    }
+}
