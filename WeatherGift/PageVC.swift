be24ecf954d41d005276a31f8917286c77ecf52f
Zach Ogden <ogdenza@bc.edu>
2019-09-26 11:11:15 -0400

    7.6 Completed

--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -58,15 +58,30 @@ class PageVC: UIPageViewController {
         
         listButton.setImage(UIImage(named: "listbutton"), for: .normal)
         listButton.setImage(UIImage(named:"listbutton-highlighted"), for: .highlighted)
-        listButton.addTarget(self, action: #selector(segueToLocationVC), for: .touchUpInside)
-        view.addSubview(pageControl)
+        listButton.addTarget(self, action: #selector(segueToListVC), for: .touchUpInside)
+        view.addSubview(listButton)
     }
     
     
-    @objc func segueToLocationVC() {
-        print("Hey you clicked me!")
+    //Mark:- Segues
+    @objc func segueToListVC() {
+        performSegue(withIdentifier: "ToListVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ToListVC" {
+            let destination = segue.destination as! ListVC
+            destination.locationsArray = locationsArray
+            destination.currentPage = currentPage
+        }
     }
 
+    
+    @IBAction func unwindFromListVC(sender: UIStoryboardSegue) {
+        pageControl.numberOfPages = locationsArray.count
+        pageControl.currentPage = currentPage
+        setViewControllers([createDetailVC(forPage: currentPage)], direction: .forward, animated: false, completion: nil)
+    }
     
     //MARK:- Create View Controller for UIPageViewController
     func createDetailVC(forPage page: Int) -> DetailVC {
