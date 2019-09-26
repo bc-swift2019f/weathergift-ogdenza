26429209bf035d6a536c85c23288ee1a37020bfc
Zach Ogden <ogdenza@bc.edu>
2019-09-26 09:52:37 -0400

    7.5 Completed

--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -12,6 +12,11 @@ class PageVC: UIPageViewController {
     
     var currentPage = 0
     var locationsArray = ["Local City", "Sydney, Australia", "Accra Ghana", "Uglich Russia"]
+    var pageControl: UIPageControl!
+    var listButton: UIButton!
+    var barButtonWidth: CGFloat = 44
+    var barButtonHeight: CGFloat = 44
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,9 +24,51 @@ class PageVC: UIPageViewController {
         dataSource = self
         
         setViewControllers([createDetailVC(forPage: 0)], direction: .forward, animated: false, completion: nil )
-
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        configurePageControl()
+        configureListButton()
+    }
+    
+    
+    //MARK:- UI Configuration Methods
+    func configurePageControl() {
+        let pageControlHeight: CGFloat = barButtonHeight
+        let pageControlWidth: CGFloat = view.frame.width - (barButtonWidth * 2)
+        
+        let safeHeight = view.frame.height - view.safeAreaInsets.bottom
+        
+        pageControl = UIPageControl(frame: CGRect(x: (view.frame.width  - pageControlWidth) / 2, y: safeHeight - pageControlHeight, width: pageControlWidth, height: pageControlHeight))
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        pageControl.currentPageIndicatorTintColor = UIColor.black
+        pageControl.numberOfPages = locationsArray.count
+        pageControl.currentPage = currentPage
+        pageControl.addTarget(self, action: #selector(pageControlPressed), for: .touchUpInside)
+        
+        view.addSubview(pageControl)
+    }
+    
+    
+    func configureListButton() {
+        let safeHeight = view.frame.height - view.safeAreaInsets.bottom
+        
+        listButton = UIButton(frame: CGRect(x: view.frame.width - barButtonWidth, y: safeHeight - barButtonHeight, width: barButtonWidth, height: barButtonHeight))
+        
+        listButton.setImage(UIImage(named: "listbutton"), for: .normal)
+        listButton.setImage(UIImage(named:"listbutton-highlighted"), for: .highlighted)
+        listButton.addTarget(self, action: #selector(segueToLocationVC), for: .touchUpInside)
+        view.addSubview(pageControl)
+    }
+    
+    
+    @objc func segueToLocationVC() {
+        print("Hey you clicked me!")
+    }
+
+    
+    //MARK:- Create View Controller for UIPageViewController
     func createDetailVC(forPage page: Int) -> DetailVC {
         currentPage = min(max(0, page), locationsArray.count - 1)
         
@@ -58,4 +105,22 @@ extension PageVC: UIPageViewControllerDelegate, UIPageViewControllerDataSource {
         }
         return nil
     }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if let currentViewController = pageViewController.viewControllers?[0] as? DetailVC {
+            pageControl.currentPage = currentViewController.currentPage
+        }
+    }
+    
+    @objc func pageControlPressed() {
+        guard let currentViewController = self.viewControllers?[0] as? DetailVC else {return}
+        currentPage = currentViewController.currentPage
+        if pageControl.currentPage < currentPage {
+            setViewControllers([createDetailVC(forPage: pageControl.currentPage)], direction: .reverse, animated: true, completion: nil)
+        } else if pageControl.currentPage > currentPage {
+            setViewControllers([createDetailVC(forPage: pageControl.currentPage)], direction: .forward, animated: true, completion: nil)
+        }
+    }
+    
+    
 }
